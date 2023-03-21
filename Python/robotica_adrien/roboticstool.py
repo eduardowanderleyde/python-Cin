d234af283c3d458ca9b7f5d18cff504430aa8783
Eduardo Wanderley de Siqueira Araújo <103546791+eduardowanderleyde@users.noreply.github.com>
2023-03-21 16:45:14 -0300

    robotcs toolbox

--- Python/robotica_adrien/roboticstool.py
+++ Python/robotica_adrien/roboticstool.py
@@ -8,8 +8,21 @@ print (robot)
 robot.plot(robot.qr)
 input ("press enter")
 
- T1 = robot.fkine([0,0])
- print(T1)
+T1 = robot.fkine([0,0])
+print(T1)
 
- T2 = robot.fkine([pi/4,pi/4])
- print (T2)
+T2 = robot.fkine([pi/4,pi/4])
+print (T2)
+
+##########
+solution1=robot.ikine_LM(T1)
+q1=solution1[0]
+print(q1)
+
+solution2=robot.ikine_LM(T2)
+q2=solution2[0]
+print(q2)
+
+T3=SE3(0.3031,11.707,0)
+solution3= robot.ikine.LM(T3)
+print(solution3)
