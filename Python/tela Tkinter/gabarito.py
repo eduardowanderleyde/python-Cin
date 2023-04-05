a411eecb9e89f0cc92d6d677d71a0b3085976160
Eduardo Wanderley de Siqueira Araújo <103546791+eduardowanderleyde@users.noreply.github.com>
2023-04-05 14:40:38 -0300

    abrindo tkinter

--- Python/tela Tkinter/gabarito.py
+++ Python/tela Tkinter/gabarito.py
@@ -1,7 +1,7 @@
-import requests
+#import requests
 from tkinter import *
 
-def pegar_cotacoes():
+"""def pegar_cotacoes():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
     requisicao_dic = requisicao.json()
@@ -13,14 +13,14 @@ def pegar_cotacoes():
     texto_resposta['text'] = f'''
     Dólar: {cotacao_dolar}
     Euro: {cotacao_euro}
-    BTC: {cotacao_btc}'''
+    BTC: {cotacao_btc}'''"""
 
 janela = Tk()
 janela.title("Cotação Atual de Moedas")
 texto = Label(janela, text="Clique no botão para ver as cotações de moedas")
 texto.grid(column=0, row=0, padx=10, pady=10)
 
-botao = Button(janela, text="Buscar cotações", command=pegar_cotacoes)
+botao = Button(janela, text="Buscar cotações")
 botao.grid(column=0, row=1, padx=10, pady=10)
 
 texto_resposta = Label(janela, text="")
